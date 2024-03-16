1de5a353ea199a660d7bddf08c3cfe98ce03981d
AniqaFarhan31 <156236517+AniqaFarhan31@users.noreply.github.com>
2024-03-16 22:40:31 +0500

    Add files via upload

---
+++ assignment12.js
@@ -0,0 +1,2 @@
+var friendsName = ["Rohaan", "Namrah", "Zubia", "Shareen"];
+friendsName.forEach(function (friendName) { return console.log("Hello ".concat(friendName, ", How are you my friend?")); });
