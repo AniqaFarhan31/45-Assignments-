6df7200cfac16bb4d97a51d109059b1db5a7ba5d
AniqaFarhan31 <156236517+AniqaFarhan31@users.noreply.github.com>
2024-03-22 06:22:26 +0500

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,2 @@
+var guestlist = ["Abeera", "Shareen", "Rohaan", "Zoha"];
+guestlist.forEach(function (oneGuest) { return console.log("Salam ".concat(oneGuest, ",Would you like to dinner with me?")); });
