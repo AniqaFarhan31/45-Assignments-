fcee0002a7bffb812e629e9e339f1db06cb40476
AniqaFarhan31 <156236517+AniqaFarhan31@users.noreply.github.com>
2024-03-22 06:23:10 +0500

    Delete assignment.js

--- assignment.js
+++
@@ -1,2 +0,0 @@
-var guestlist = ["Abeera", "Shareen", "Rohaan", "Zoha"];
-guestlist.forEach(function (oneGuest) { return console.log("Salam ".concat(oneGuest, ",Would you like to dinner with me?")); });
