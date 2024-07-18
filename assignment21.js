7aa41a4148b09ea68383752276f88a01f7e067f3
AniqaFarhan31 <156236517+AniqaFarhan31@users.noreply.github.com>
2024-07-18 14:56:24 -0700

    Add files via upload

---
+++ assignment21.js
@@ -0,0 +1,7 @@
+var studentBioData = {
+    studentName: "Aniqa Farhan",
+    studentAge: "18",
+    studentFatherName: "Muhammad Farhan",
+    studentMotherName: "Afshan Farhan",
+};
+console.log(studentBioData);
