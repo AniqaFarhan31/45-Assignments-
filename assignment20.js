310a4c9e1199850bbd45e75ebd43cda757885eec
AniqaFarhan31 <156236517+AniqaFarhan31@users.noreply.github.com>
2024-07-18 14:42:04 -0700

    Add files via upload

---
+++ assignment20.js
@@ -0,0 +1,3 @@
+var coloursOfRainbow = ["Violet", "Indigo", "Blue", "Green", "Yellow", "Orange", "Red"];
+console.log("List of Colours Of Rainbow:");
+coloursOfRainbow.forEach(function (colour) { return console.log(colour); });
