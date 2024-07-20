6b6f82b29f728870e0abdde4ce15376b26808d1c
AniqaFarhan31 <156236517+AniqaFarhan31@users.noreply.github.com>
2024-07-20 14:22:00 -0700

    Delete assignment20.js

--- assignment20.js
+++
@@ -1,3 +0,0 @@
-var coloursOfRainbow = ["Violet", "Indigo", "Blue", "Green", "Yellow", "Orange", "Red"];
-console.log("List of Colours Of Rainbow:");
-coloursOfRainbow.forEach(function (colour) { return console.log(colour); });
