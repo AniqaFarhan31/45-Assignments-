3131cc6302ce4e695501c8c8ab31dde095474c3b
AniqaFarhan31 <156236517+AniqaFarhan31@users.noreply.github.com>
2024-07-20 14:21:47 -0700

    Delete assignment19.js

--- assignment19.js
+++
@@ -1,4 +0,0 @@
-var guestList = ["Abeera", "Shareen", "Rohaan", "Zoha"];
-//guestList.forEach(oneguest => console.log(`Salam ${oneguest}, Would you like to dinner with me?`));
-var lengthGuests = guestList.length;
-console.log("We are inviting total ".concat(lengthGuests, " guests."));
