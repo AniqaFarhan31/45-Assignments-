a4b99449dba9bf41f93d3929a71edff4c6d7afa5
AniqaFarhan31 <156236517+AniqaFarhan31@users.noreply.github.com>
2024-07-20 15:40:20 -0700

    Add files via upload

---
+++ assignment19.js
@@ -0,0 +1,4 @@
+var guestList = ["Abeera", "Shareen", "Rohaan", "Zoha"];
+//guestList.forEach(oneguest => console.log(`Salam ${oneguest}, Would you like to dinner with me?`));
+var lengthGuests = guestList.length;
+console.log("We are inviting total ".concat(lengthGuests, " guests."));
